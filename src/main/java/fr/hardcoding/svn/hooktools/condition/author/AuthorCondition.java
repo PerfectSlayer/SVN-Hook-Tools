371616ad35c7ebcc324eec005bc93cf1ec69223d
PerfectSlayer <bruce.bujon@gmail.com>
2014-04-26 16:44:36 +0200

    Improve commit author condition

--- src/main/java/fr/hardcoding/svn/hooktools/condition/author/AuthorCondition.java
+++ src/main/java/fr/hardcoding/svn/hooktools/condition/author/AuthorCondition.java
@@ -1,6 +1,7 @@
 package fr.hardcoding.svn.hooktools.condition.author;
 
 import fr.hardcoding.svn.hooktools.condition.AbstractCondition;
+import fr.hardcoding.svn.hooktools.condition.StringComparison;
 import fr.hardcoding.svn.hooktools.configuration.ConfigurationParameter;
 import fr.hardcoding.svn.hooktools.hook.AbstractHook;
 import fr.hardcoding.svn.hooktools.hook.UnavailableHookDataException;
@@ -15,19 +16,23 @@ public class AuthorCondition extends AbstractCondition {
 	/** The requested commit author name. */
 	@ConfigurationParameter(isRequired = true)
 	public String name;
+	/** The author name comparison. */
+	@ConfigurationParameter
+	public StringComparison nameComparison;
 
 	/**
 	 * Constructor.
 	 */
 	public AuthorCondition() {
-
+		// Set default name comparison
+		this.nameComparison = StringComparison.IS;
 	}
 
 	@Override
 	public boolean check(AbstractHook hook) {
 		try {
 			// Check commit author with requested commit author
-			return hook.getCommitAuthor().equals(this.name);
+			return this.nameComparison.compare(hook.getCommitAuthor(), this.name);
 		} catch (UnavailableHookDataException exception) {
 			// Invalidate the operation if data are not available
 			return false;
